d309f12d42fbcbb9b9b89ccffab75e272ab33c34
Otto Lote <ottolote93@gmail.com>
2016-01-22 08:12:27 +0100

    Update answer to EX1.4 implemented in Go  Changes to be committed: 	modified:   Ex1_4.go

--- exercises/Ex01-Intro/Ex1_4.go
+++ exercises/Ex01-Intro/Ex1_4.go
@@ -9,17 +9,28 @@ import (
     "time"
 )
 
-func someGoroutine() {
-    Println("Hello from a goroutine!")
+var i int
+
+func increase_i() {
+		for j := 0; j < 1000000; j++ {
+				i++
+		}
+}
+
+func decrease_i() {
+		for j := 0; j < 1000000; j++ {
+				i--
+		}
 }
 
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
                                             // Try doing the exercise both with and without it!
-    go someGoroutine()                      // This spawns someGoroutine() as a goroutine
+    go increase_i()                      // This spawns someGoroutine() as a goroutine
+    go decrease_i()                      // This spawns someGoroutine() as a goroutine
 
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
-    Println("Hello from main!")
+    Println(i)
 }
