57a89056d2dc784969cfa6ed93a191a257c75061
Otto Lote <ottolote93@gmail.com>
2016-01-22 09:33:09 +0100

    Implement Ex2.go with sleep to join threads  Changes to be committed: 	modified:   Ex2.go           - Could be improved by using sync

--- exercises/Ex02-Bottlenecks/Ex2.go
+++ exercises/Ex02-Bottlenecks/Ex2.go
@@ -6,20 +6,26 @@ package main
 import (
     . "fmt"
     "runtime"
+    "sync"
     "time"
 )
 
 var i int
+var mutex_i = &sync.Mutex{}
 
 func increase_i() {
 		for j := 0; j < 1000000; j++ {
+				mutex_i.Lock()
 				i++
+				mutex_i.Unlock()
 		}
 }
 
 func decrease_i() {
 		for j := 0; j < 1000000; j++ {
+				mutex_i.Lock()
 				i--
+				mutex_i.Unlock()
 		}
 }
 
@@ -31,6 +37,6 @@ func main() {
 
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
-    time.Sleep(100*time.Millisecond)
+    time.Sleep(1000*time.Millisecond) // Lazy joining of threads #SWAG
     Println(i)
 }
