e96e7a394aa18e88c27166ff05c4a9d42710b022
Otto Lote <ottolote93@gmail.com>
2016-01-22 09:26:27 +0100

    Implement horribly slow mutex in Ex2.py  Changes to be committed: 	modified:   Ex2.py

--- exercises/Ex02-Bottlenecks/Ex2.py
+++ exercises/Ex02-Bottlenecks/Ex2.py
@@ -1,15 +1,21 @@
 import threading
 i = 0
 
+mutex_i = threading.Lock() #same as threading.Semaphore(), but Semaphore was HORRIBLY SLOW. Lock() is not really much better
+
 def func1():
 	global i
 	for x in range(1000000):
+        	mutex_i.acquire()
 		i += 1
+                mutex_i.release()
 
 def func2():
 	global i
 	for x in range(1000000):
+        	mutex_i.acquire()
 		i -= 1
+                mutex_i.release()
 
 thread1 = threading.Thread(name='func1', target=func1)
 thread2 = threading.Thread(name='func2', target=func2)
