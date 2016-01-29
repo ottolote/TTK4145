9bcf4ee64e79338619326781f705f7b156f4b493
edvardsp <edvard.pettersen@gmail.com>
2016-01-29 15:50:44 +0100

    Remove IPv6 support as it is not used in this project and clutters the code  Changes to be committed: 	modified:   Makefile             - cleanup 	modified:   networkhandler.c             - remove IPv6 support for simplicity 	modified:   networkhandler.h             - remove IPv6-function get_in_addr()

--- exercises/Ex04-Network-module/networkhandler.c
+++ exercises/Ex04-Network-module/networkhandler.c
@@ -5,7 +5,7 @@
 // Written using this great guide:
 // http://www.beej.us/guide/bgnet/output/html/singlepage/bgnet.html
 //
-// Works with both IPv4 and IPv6
+// Works with only IPv4
 //
 // TODO:
 //  - Message passing
@@ -28,44 +28,39 @@
 #define MAXBUFLEN 549 // could be bigger, 549 was good according to random site
 #define MYPORT "30000"
 
-// FOR EPIC COLORS
-#define NETWORKSTRING "[ " TCOLOR_BLUE "networkhandler" TCOLOR_NC " ]: "
 
-
-// Private functions:
-// get sockaddress IPv4 or IPv6
-void *get_in_addr(struct sockaddr *sa);
+// FOR EPIC COLOROUTPUT
+#define NETWORKSTRING "[ " TCOLOR_LIGHTBLUE "NetworkHandler" TCOLOR_NC " ] - "
 
 
 
 
-
-void* networkHandlerRoutine() { // running as thread
+void* networkHandlerRoutine() {  
   // INIT //
 
   // Initialize structs of type addrinfo
   // - hints:     for specifying initial options
   // - servinfo.  using specified hints to fill in possible servinfo
-  // servinfo is pointer to first element in linked list of possible servinfo
+  // servinfo is pointer to first element in linked list of possible addrinfo
   struct addrinfo hints, *servinfo; 
   memset(&hints, 0, sizeof hints);
 
-  hints.ai_family = AF_UNSPEC; // Use IPv4 or IPv6, AF_INET for forced IPv4
+  hints.ai_family = AF_INET; // AF_INET for forced IPv4
   hints.ai_socktype = SOCK_DGRAM; // Only listen for datagrams
-  hints.ai_flags = AI_PASSIVE; // Set ip to my ip (for server socket)
+  hints.ai_flags = AI_PASSIVE; // Set addrinfo with 'my ip' (for server socket)
 
 
 
   int retval;
   // Evaluation of assignment is equal to left operand after assignment
-  if ( (retval = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0 ) {
+  if ((retval = getaddrinfo(NULL, MYPORT, &hints, &servinfo)) != 0) {
     fprintf(stderr, "networkhandler: getaddrinfo: %s\n", gai_strerror(retval));
     return NULL;
   }
 
 
   // loop through linked list and bind to first possible
-  struct addrinfo *p;
+  struct addrinfo *p; // p is pointer to 
   int sockfd;
   for (p = servinfo; p != NULL; p = p->ai_next) {
 
@@ -75,7 +70,7 @@ void* networkHandlerRoutine() { // running as thread
       continue;
     }
 
-    // Try to bind socket to address
+    // Try to bind socket to addressinfo (includes port information)
     if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1 ) {
       close(sockfd);
       perror("bind");
@@ -83,7 +78,8 @@ void* networkHandlerRoutine() { // running as thread
     }
 
     break; //break if successful
-  }
+  } //end of for-loop
+
 
   if(p == NULL) {
     fprintf(stderr, "networkhandler: failed to bind to socket");
@@ -99,24 +95,21 @@ void* networkHandlerRoutine() { // running as thread
   // LOOP //
 
   char buf[MAXBUFLEN]; // Initialize receive buffer
-  struct sockaddr_storage senders_addr; // struct to store IPv4 sockaddr OR IPv6 sockaddr from sender
+  struct sockaddr senders_addr; // struct to store socketinfo from senders addr 
   socklen_t addr_len = sizeof senders_addr;
   char addr_str[INET6_ADDRSTRLEN]; 
   int byteCount;
 
 
   // Fill receivebuffer with message, print buffer, repeat
+  // USING BLOCKING SOCKET
   while(1) {
     printf( NETWORKSTRING "Waiting for datagram packets at port %s\n", MYPORT);
 
 
     // - recvfrom receives packet and stores sender's address
-    // - sockaddr_storage (senders_addr) can be cast to sockaddr 
-    //      and both IPv4 and IPv6 information is magically cast 
-    //      correctly into sockaddr and addr_len automatically
-    //      reduced if truncation is detected
     if((byteCount = recvfrom(sockfd, buf, MAXBUFLEN-1, 0,
-            (struct sockaddr*)&senders_addr, &addr_len)) == -1) {
+            &senders_addr, &addr_len)) == -1) {
       perror("recvfrom");
       exit(1);
     }
@@ -125,11 +118,10 @@ void* networkHandlerRoutine() { // running as thread
     // Print packet info and message
     buf[byteCount] = '\0'; // Add nullcharacter to buffer for printf
     printf( NETWORKSTRING "received packet from: %s\n", 
-        inet_ntop(senders_addr.ss_family, 
-          get_in_addr((struct sockaddr*)&senders_addr),
-            addr_str, sizeof addr_str));
+        inet_ntop(senders_addr.sa_family, 
+            senders_addr.sa_data, addr_str, sizeof addr_str));
     printf( NETWORKSTRING "packet is %d bytes long\n", byteCount);
-    printf( NETWORKSTRING "packet contains: \"%s\"\n", buf);
+    printf( NETWORKSTRING "packet contains: \"%s\"\n\n", buf);
   } // end of while
 
 
@@ -137,13 +129,3 @@ void* networkHandlerRoutine() { // running as thread
   return 0;
 }
 
-// From www.beej.us
-// Check if IPv4 or IPv6 and cast accordingly
-void *get_in_addr(struct sockaddr *sa)
-{
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
-}
