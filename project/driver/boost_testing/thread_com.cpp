5c0a97ff07dc58d54a5f46d12513d07c41627eeb
Otto Lote <ottolote93@gmail.com>
2016-03-02 18:51:25 +0100

    Network class framework with timeout timer works (no actual networking set up)

--- project/driver/boost_testing/thread_com.cpp
+++ project/driver/boost_testing/thread_com.cpp
@@ -1,46 +1,68 @@
 #include <iostream>
+#include <unistd.h>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
 
+using namespace boost;
+
 class Network {
     public:
-//         Network() 
-//             : timeout_timer(io, boost::posix_time::seconds(1)) 
-//         {
-//                     timeout_timer.async_wait([=] { this->doStuff();});
-// 
-//         }
-
+        Network() 
+            : timeout_timer(io, posix_time::milliseconds(1000))
+        {
+            timeout_timer.async_wait(bind(&Network::timeout, this, 
+                        asio::placeholders::error));
+        }
 
         void run() {
             io.run();
+            std::cout << "networkthread reached end\n";
+        }
+
+        void refresh_timeout_timer() {
+            timeout_timer.cancel(); 
+            timeout_timer.expires_from_now(posix_time::milliseconds(1000));
+            timeout_timer.async_wait(bind(&Network::timeout,this,
+                        asio::placeholders::error));
         }
 
         void doStuff() {
-                std::cout << "handler schmandler\n";
+            //if(e) { std::cout << "wtf\n"; }
+            std::cout << "handler schmandler is doing stuff\n";
+            refresh_timeout_timer();
         }
 
         void post() {
-                io.post([=] { Network::doStuff();});
+            io.post(bind(&Network::doStuff, this));
         }
-    private:
-            boost::asio::io_service io;
 
-//            boost::asio::deadline_timer timeout_timer;
+    private:
+        asio::io_service io;
+        asio::deadline_timer timeout_timer;
+
+        void timeout(const system::error_code &e) {
+            // don't do anything if the timeout was aborted
+            if (e == asio::error::operation_aborted) {return;}
+
+            std::cout << "NetworkThread timed out, we should probably tell main something is wrong\n";
+        }
 
 }; //end of class Network
-
 
 
 int main() {
 
     Network Networkhandler;
-    Networkhandler.post();
+    thread NetworkThread(bind( &Network::run, &Networkhandler));
 
-    boost::thread t([=] { Network::run();})
-        return 0;
+    for (int i = 0; i<8; i++) {
+        Networkhandler.post();
+        usleep(500000);
+    }
 
+    NetworkThread.join();
+    return 0;
 }
 
