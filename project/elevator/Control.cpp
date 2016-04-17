70a2352b00463a02163865835b5f609bb41d5d9d
Otto Lote <ottolote93@gmail.com>
2016-04-17 20:33:07 +0200

    add print functions

--- project/elevator/Control.cpp
+++ project/elevator/Control.cpp
@@ -675,6 +675,6 @@ void Control::print_pending_orders() {
     for (int i = 0; i<N_ORDER_BUTTONS; i++) {
         std::cout << pending_orders[i] << "\t";
     }
-    std::cout << "\n1U\t2D\t2U\t3D\t3U\t4D\tF1\tF2\tF3\tF4\n-------------------------------------------------------------------------\n" << std::endl;
+    std::cout << "\n1U\t2D\t2U\t3D\t3U\t4D\tF1\tF2\tF3\tF4\n----------------------------------------------------------------------------\n" << std::endl;
 
 }
